11c7918403f7ba22fa53522dca4249da403aa76f
Zeereoreo <wltjrcjf12@naver.com>
2025-03-10 14:34:35 +0900

    feat : vercel 오류수정

--- src/components/header/header.view.tsx
+++ src/components/header/header.view.tsx
@@ -1,15 +1,12 @@
-
-
 import { useHeaderHook } from "./header.hook";
 import { HeaderStyle } from "./header.style";
 
 export default function HeaderView() {
-    const { onClickedMainPage } = useHeaderHook();
-
+    const { onClickHome } = useHeaderHook();
 
     return (
         <HeaderStyle.Container>
-            <HeaderStyle.Logo.Container onClick={onClickedMainPage}>
+            <HeaderStyle.Logo.Container onClick={onClickHome}>
                 <HeaderStyle.Logo.Image>
                     Logo
                 </HeaderStyle.Logo.Image>
@@ -20,5 +17,5 @@ export default function HeaderView() {
                 </HeaderStyle.Login.Image>
             </HeaderStyle.Login.Container>
         </HeaderStyle.Container>
-    )
+    );
 }
